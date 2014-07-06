b1def86cb957acf2677fd85252322cfe917f2f61
Xavier Barbosa <xavier.barbosa@iscool-e.com>
2014-07-06 14:24:39 +0200

    first it

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    setup_requires=['d2to1'],
+    tests_require=[],
+    d2to1=True
+)
---
+++ tablette/utils.py
@@ -0,0 +1,12 @@
+__all__ = ['terminal_size']
+
+import fcntl, termios, struct
+
+
+def terminal_size():
+    """Returns width and height of the console.
+    """
+    height, width, hp, wp = struct.unpack('HHHH',
+        fcntl.ioctl(0, termios.TIOCGWINSZ,
+        struct.pack('HHHH', 0, 0, 0, 0)))
+    return width, height
